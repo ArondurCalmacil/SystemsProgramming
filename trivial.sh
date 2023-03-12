0bfbc27547e12357b54e6091c13650b8625719b9
Arondur <easwar.allada@gmail.com>
2023-03-12 18:50:50 +1030

    changes

--- trivial.sh
+++ trivial.sh
@@ -3,7 +3,16 @@ chmod +x trivial.sh
 i=1
 for arg in "$@"; do
     echo "$i= $arg"
-    i=$((i + 1))
+    ((i++))
+    if [ $i -gt 9 ]; then
+        break
+    fi
+done
+
+while [ $i -lt 10 ]
+do
+    echo "$i= "
+    ((i++))
 done
 
 # The difference between ./trivial.sh "This is a string" '$1' and ./trivial.sh "This is a string" "$1"
